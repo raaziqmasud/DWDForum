db750be4c847e1ccc95cd5e95f6dbf071f3b939d
Raaziq Brown <raaziqbrown@10-17-208-132.dynapool.wireless.nyu.edu>
2019-04-21 14:50:37 -0400

    yay

--- index.js
+++ index.js
@@ -43,9 +43,9 @@ app.get('/', function(req, res) {
     });
   });
 
-app.listen(7000, function () {
-  console.log('Example app listening on port 7000!')
-})
+// app.listen(7000, function () {
+//   console.log('Example app listening on port 7000!')
+// })
 
 app.listen(process.env.PORT || 7000, function () {
     console.log('Example app listening on port 7000!')
