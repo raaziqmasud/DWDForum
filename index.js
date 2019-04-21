d5c43f3628d46c62b9447fd68a9db6b87a7e4245
Raaziq Brown <raaziqbrown@10-17-208-132.dynapool.wireless.nyu.edu>
2019-04-21 15:07:07 -0400

    here

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express')
 var app = express()
 var cors = require('cors')
-var mustacheExpress = require('mustache-express')
+var mustachE = require('mustache-express')
  
 app.use(cors())
 
@@ -52,11 +52,5 @@ app.listen(process.env.PORT || 7000, function () {
     console.log('Example app listening on port 7000!')
   })
 
-// // Create a JavaScript Object with data to store
-// var datatosave = {
-// 	name: "Shawn",
-// 	message: "Hello world"
-// };
-		
 
 
